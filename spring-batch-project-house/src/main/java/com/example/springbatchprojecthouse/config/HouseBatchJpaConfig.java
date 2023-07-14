a8e54bba299717f2fdadb79ad6d7d6e0b4d9f901
renechoi <115696395+renechoi@users.noreply.github.com>
2023-07-14 15:30:47 +0900

    feat: batch-house 프로젝트 셋업

---
+++ spring-batch-project-house/src/main/java/com/example/springbatchprojecthouse/config/HouseBatchJpaConfig.java
@@ -0,0 +1,9 @@
+package com.example.springbatchprojecthouse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class HouseBatchJpaConfig {
+}
