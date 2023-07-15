20448d382e5702d0e2f9defc5c6a68d09660533d
renechoi <115696395+renechoi@users.noreply.github.com>
2023-07-15 13:43:31 +0900

    feat: batch-house apt, aptdeal 엔티티 및 dto 작성

---
+++ spring-batch-project-house/src/main/java/com/example/springbatchprojecthouse/core/dto/AptDto.java
@@ -0,0 +1,11 @@
+package com.example.springbatchprojecthouse.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class AptDto {
+    private String name;
+    private Long price;
+}
