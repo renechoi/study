3b6ca3eb3205096d8555fcd618622f02dbcfe881
renechoi <115696395+renechoi@users.noreply.github.com>
2023-07-17 21:11:12 +0900

    feat: 동시성 이슈2 프로젝트 셋업

---
+++ concurrency-study-2/src/main/java/com/example/concurrencystudy2/ConcurrencyStudy2Application.java
@@ -0,0 +1,13 @@
+package com.example.concurrencystudy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyStudy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyStudy2Application.class, args);
+	}
+
+}
