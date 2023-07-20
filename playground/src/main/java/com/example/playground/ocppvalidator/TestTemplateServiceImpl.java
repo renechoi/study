223f0684665c56844ec9d3c1c2ff5b67e5872bd4
renechoi <115696395+renechoi@users.noreply.github.com>
2023-07-20 20:08:44 +0900

    feat: 테스트 템플릿 자료 구조 변경

--- playground/src/main/java/com/example/playground/ocppvalidator/TestTemplateServiceImpl.java
+++ playground/src/main/java/com/example/playground/ocppvalidator/TestTemplateServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.playground.ocppvalidator;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,15 +8,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TestTemplateServiceImpl implements TestTemplateService {
-    private Map<Integer, TestTemplate> templates = new HashMap<>();
+	private Map<Integer, TestTemplate> templates = new LinkedHashMap<>();
 
-    @Override
-    public List<TestTemplate> registerTemplates(List<TestTemplate> testTemplates) {
-        testTemplates.forEach(template -> templates.put(template.getSequence(), template));
-        return testTemplates;
-    }
+	@Override
+	public List<TestTemplate> registerTemplates(List<TestTemplate> testTemplates) {
+		testTemplates.forEach(template -> templates.put(template.getSequence(), template));
+		return testTemplates;
+	}
 
-    public TestTemplate getTemplate(Integer sequence) {
-        return templates.get(sequence);
-    }
+	public TestTemplate getTemplate(Integer sequence) {
+		return templates.get(sequence);
+	}
 }
