605de9b7530a84e4173ff544add1855fe80ccb16
renechoi <115696395+renechoi@users.noreply.github.com>
2023-07-25 23:52:18 +0900

    feat: member 코드 작성

---
+++ rest-docs-practice/src/main/java/com/example/restdocspractice/member/MemberSignUpRequest.java
@@ -0,0 +1,24 @@
+package com.example.restdocspractice.member;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class MemberSignUpRequest {
+
+    @Email
+    @Size(max = 30)
+    private String email;
+
+    @NotEmpty
+    @Size(max = 10)
+    private String name;
+
+    private MemberStatus status;
+
+    public Member toEntity() {
+        return new Member(email, name, status);
+    }
+}
