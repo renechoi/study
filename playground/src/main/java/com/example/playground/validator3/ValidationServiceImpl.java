c41eea07d849c341c51aba472696d6f39d2e0f25
renechoi <115696395+renechoi@users.noreply.github.com>
2023-07-27 23:35:44 +0900

    feat: validator 체인 방식 연구

---
+++ playground/src/main/java/com/example/playground/validator3/ValidationServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.playground.validator3;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.playground.ocppvalidator.TestTemplate;
+import com.example.playground.ocppvalidator.validation.ValidationService;
+
+@Service
+public class ValidationServiceImpl implements ValidationService {
+    // ...
+
+    @Override
+    public void validateTemplateWithLogEvents(List<LogEvent> logEvents, List<TestTemplate> templates) {
+        for (TestTemplate template : templates) {
+            boolean validationSucceeded = validators.get(0).given(template).whenTestedWith(logEvents).thenValidate();
+            if (!validationSucceeded) {
+                System.out.println("Validation failed for template: " + template);
+            }
+        }
+    }
+}
---
+++ playground/src/main/java/com/example/playground/validator3/Validator.java
@@ -0,0 +1,13 @@
+package com.example.playground.validator3;
+
+import com.example.playground.ocppvalidator.TestTemplate;
+
+public interface Validator {
+    boolean supports(TestTemplate testTemplate);
+    Validator given(TestTemplate testTemplate);
+    Validator whenTestedWith(List<LogEvent> logEvents);
+    boolean thenValidate();
+    Validator setNext(Validator nextValidator);
+    Validator getNext();
+    ResultRecorder getRecorder();
+}
