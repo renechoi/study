19681b27b76da14a4865d2888953318edb02b84d
renechoi <115696395+renechoi@users.noreply.github.com>
2023-08-14 22:42:19 +0900

    feat: starbucks practice header badge scroll 기능 구현

--- front/practice/starbucks-clone-practice/js/main.js
+++ front/practice/starbucks-clone-practice/js/main.js
@@ -14,3 +14,19 @@ searchInputEl.addEventListener('blur', function (){
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+
+const badgeEl = document.querySelector('header .badges');
+window.addEventListener('scroll', _.throttle(function () {
+    if (window.scrollY > 500){
+        gsap.to(badgeEl, .6, {
+            opacity:0,
+            display: 'none'
+        })
+    } else{
+        gsap.to(badgeEl, .6, {
+            opacity:1,
+            display: 'block'
+        })
+    }
+}, 300))
