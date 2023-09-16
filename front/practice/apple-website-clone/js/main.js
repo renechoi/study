1729d7e7aa86884c1e815ba341a6029312042abe
renechoi <115696395+renechoi@users.noreply.github.com>
2023-09-16 19:51:18 +0900

    feat: media query 반영 반응형 모드 최적화 구현

--- front/practice/apple-website-clone/js/main.js
+++ front/practice/apple-website-clone/js/main.js
@@ -1,4 +1,3 @@
-
 import ipads from '../data/ipads.js'
 import navigations from '../data/navigations.js'
 
@@ -33,7 +32,7 @@ function hideBasket() {
 }
 
 
-// 검색 ! 
+// 헤더 검색!
 // 헤더 검색 관련 요소 찾기.
 const headerEl = document.querySelector('header')
 const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]
@@ -44,7 +43,6 @@ const searchShadowEl = searchWrapEl.querySelector('.shadow')
 const searchInputEl = searchWrapEl.querySelector('input')
 const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
 const duration = .4 // 초(seconds) 단위, 시간을 변수에 저장해서 사용하면 쉽게 관리 용이
-
 
 searchStarterEl.addEventListener('click', showSearch)
 searchCloserEl.addEventListener('click', event => {
@@ -88,6 +86,63 @@ function stopScroll() {
     document.documentElement.classList.add('fixed')
 }
 
+// 헤더 메뉴 토글! [모바일]
+const menuStarterEl = document.querySelector('header .menu-starter')
+menuStarterEl.addEventListener('click', () => {
+    if (headerEl.classList.contains('menuing')) {
+        headerEl.classList.remove('menuing')
+        searchInputEl.value = ''
+        playScroll()
+    } else {
+        headerEl.classList.add('menuing')
+        stopScroll()
+    }
+})
+
+// 헤더 검색! [모바일]
+const searchTextFieldEl = document.querySelector('header .textfield')
+const searchCancelEl = document.querySelector('header .search-canceler')
+searchTextFieldEl.addEventListener('click', () => {
+    headerEl.classList.add('searching--mobile')
+    searchInputEl.focus()
+})
+searchCancelEl.addEventListener('click', () => {
+    headerEl.classList.remove('searching--mobile')
+})
+
+// 화면 크기가 달라졌을 때 검색 모드가 종료되도록 처리.
+window.addEventListener('resize', event => {
+    if (window.innerWidth <= 740) {
+        headerEl.classList.remove('searching')
+    } else {
+        headerEl.classList.remove('searching--mobile')
+    }
+})
+
+
+// 네비게이션 메뉴 토글! [모바일]
+const navEl = document.querySelector('nav')
+const navMenuToggleEl = navEl.querySelector('.menu-toggler')
+const navMenuShadowEl = navEl.querySelector('.shadow')
+navMenuToggleEl.addEventListener('click', () => {
+    if (navEl.classList.contains('menuing')) {
+        hideNavMenu()
+    } else {
+        showNavMenu()
+    }
+})
+navEl.addEventListener('click', event => {
+    event.stopPropagation()
+})
+navMenuShadowEl.addEventListener('click', hideNavMenu)
+window.addEventListener('click', hideNavMenu)
+function showNavMenu() {
+    navEl.classList.add('menuing')
+}
+function hideNavMenu() {
+    navEl.classList.remove('menuing')
+}
+
 
 // 요소의 가시성 관찰 로직!
 const io = new IntersectionObserver(entries => {
@@ -104,8 +159,6 @@ const io = new IntersectionObserver(entries => {
 const infoEls = document.querySelectorAll('.info')
 // 관찰 시작!
 infoEls.forEach(el => io.observe(el))
-
-
 
 
 // 비디오 재생!
@@ -158,8 +211,6 @@ ipads.forEach(ipad => {
 
     itemsEl.append(itemEl)
 })
-
-
 
 
 // 푸터 내비게이션 맵 랜더링!
