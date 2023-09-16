c3ca9f86787c9df157b8aac4631ea73ccaf36e91
renechoi <115696395+renechoi@users.noreply.github.com>
2023-09-16 12:20:34 +0900

    feat: 기타 섹션들 추가

--- front/practice/apple-website-clone/js/main.js
+++ front/practice/apple-website-clone/js/main.js
@@ -1,5 +1,6 @@
 
-
+import ipads from '../data/ipads.js'
+import navigations from '../data/navigations.js'
 
 
 // 장바구니!
@@ -103,3 +104,57 @@ const io = new IntersectionObserver(entries => {
 const infoEls = document.querySelectorAll('.info')
 // 관찰 시작!
 infoEls.forEach(el => io.observe(el))
+
+
+
+
+// 비디오 재생!
+const video = document.querySelector('.stage video')
+const playBtn = document.querySelector('.stage .controller--play')
+const pauseBtn = document.querySelector('.stage .controller--pause')
+
+// Google 자동 재생 정책 확인! - https://developer.chrome.com/blog/autoplay/#audiovideo-elements
+// video.play()
+//   .then(played)
+//   .catch(paused)
+
+playBtn.addEventListener('click', () => {
+    video.play()
+    playBtn.classList.add('hide')
+    pauseBtn.classList.remove('hide')
+})
+pauseBtn.addEventListener('click', () => {
+    video.pause()
+    playBtn.classList.remove('hide')
+    pauseBtn.classList.add('hide')
+})
+
+
+// '당신에게 맞는 iPad는?' 랜더링!
+const itemsEl = document.querySelector('section.compare .items')
+ipads.forEach(ipad => {
+    const itemEl = document.createElement('div')
+    itemEl.classList.add('item')
+
+    let colorList = ''
+    ipad.colors.forEach(color => {
+        colorList += `<li style="background-color: ${color};"></li>`
+    })
+
+    // VS Code 확장 프로그램 - Comment tagged templates
+    itemEl.innerHTML = /* html */ `
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩${ipad.price.toLocaleString('en-US')}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  `
+
+    itemsEl.append(itemEl)
+})
