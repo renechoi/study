eb12fc9741716aca9cd3572a2021b226519c29f3
renechoi <115696395+renechoi@users.noreply.github.com>
2023-10-03 17:06:28 +0900

    feat: component state management

--- front/practice/vanilla-js-movie-app/dist/index.4d6bcbeb.js
+++ front/practice/vanilla-js-movie-app/dist/index.4d6bcbeb.js
@@ -603,6 +603,8 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "Component", ()=>Component);
 parcelHelpers.export(exports, "createRouter", ()=>createRouter);
+///// Store /////
+parcelHelpers.export(exports, "Store", ()=>Store);
 class Component {
     constructor(payload = {}){
         const { tagName = "div", state = {} } = payload;
@@ -643,6 +645,38 @@ function createRouter(routes) {
         });
         routeRender(routes);
     };
+}
+class Store {
+    constructor(state){
+        this.state = {} // 상태(데이터)
+        ;
+        this.observers = {};
+        for(const key in state)// 각 상태에 대한 변경 감시(Setter) 설정!
+        Object.defineProperty(this.state, key, {
+            // Getter
+            get: ()=>state[key],
+            // Setter
+            set: (val)=>{
+                state[key] = val;
+                if (Array.isArray(this.observers[key])) this.observers[key].forEach((observer)=>observer(val));
+            }
+        });
+    }
+    // 상태 변경 구독!
+    subscribe(key, cb) {
+        Array.isArray(this.observers[key]) // 이미 등록된 콜백이 있는지 확인!
+         ? this.observers[key].push(cb) // 있으면 새로운 콜백 밀어넣기!
+         : this.observers[key] = [
+            cb
+        ] // 없으면 콜백 배열로 할당!
+        ;
+    // 예시)
+    // observers = {
+    //   구독할상태이름: [실행할콜백1, 실행할콜백2]
+    //   movies: [cb, cb, cb],
+    //   message: [cb]
+    // }
+    }
 }
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"gkKU3":[function(require,module,exports) {
