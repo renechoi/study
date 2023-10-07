3b843ccc756e70f0a3d6c8196918cc92d2f652d3
renechoi <115696395+renechoi@users.noreply.github.com>
2023-10-07 14:13:10 +0900

    feat: about js -> typescript

--- front/practice/vanilla-js-movie-app/src/routes/About.ts
+++ front/practice/vanilla-js-movie-app/src/routes/About.ts
@@ -1,8 +1,9 @@
 import { Component } from '../core/heropy'
+import aboutStore from '../store/about'
 
 export default class About extends Component {
     render() {
-        const { photo, name, email, github, blog } = history.state
+        const { photo, name, email, github, blog } = aboutStore.state
 
         this.el.classList.add('container', 'about')
         this.el.innerHTML = /* html */ `
