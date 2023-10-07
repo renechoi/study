a82e95cec52a4226cf2e609d61d14cee46a82a72
renechoi <115696395+renechoi@users.noreply.github.com>
2023-10-07 21:00:01 +0900

    feat: react tictactoe app project setup and implement basic components

---
+++ front/practice/react-tictactoe-app/src/components/Square.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './Square.css';
+class Square extends Component {
+
+    render() {
+        return (
+            <button className="square">
+            </button>
+        );
+    }
+}
+
+export default Square;
