0375e96d758cddc1fec689f5cb46074472230ff5
renechoi <115696395+renechoi@users.noreply.github.com>
2023-10-08 11:39:11 +0900

    feat: react hook을 이용한 함수형 컴포넌트 변경

--- front/practice/react-tictactoe-app/src/components/Square.js
+++ front/practice/react-tictactoe-app/src/components/Square.js
@@ -1,18 +1,15 @@
 import React, {Component} from 'react';
 import './Square.css';
 
-class Square extends Component {
+const Square = ({onclick, value}) => {
 
 
-
-    render() {
-        return (
-            <button className="square"
-                    onClick={() => this.props.onclick()}>
-                {this.props.value}
-            </button>
-        );
-    }
+    return (
+        <button className="square"
+                onClick={() => onclick()}>
+            {value}
+        </button>
+    );
 }
 
 export default Square;
