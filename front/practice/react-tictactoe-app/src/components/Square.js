08071cc311c905f9b0e4fbc70ff0f8476ded1bad
renechoi <115696395+renechoi@users.noreply.github.com>
2023-10-08 11:30:18 +0900

    feat: state management

--- front/practice/react-tictactoe-app/src/components/Square.js
+++ front/practice/react-tictactoe-app/src/components/Square.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import './Square.css';
+
 class Square extends Component {
+
+
 
     render() {
         return (
-            <button className="square">
+            <button className="square"
+                    onClick={() => this.props.onclick()}>
                 {this.props.value}
             </button>
         );
