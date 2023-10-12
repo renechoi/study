dd3bc0cb536cd245d21472c833f469154e830796
renechoi <115696395+renechoi@users.noreply.github.com>
2023-10-12 23:21:50 +0900

    feat: category 구성 및 스타일링

--- front/practice/react-disney-app/src/App.js
+++ front/practice/react-disney-app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Nav from "./components/Nav";
 import styled from "styled-components";
 import Banner from "./components/Banner";
+import Category from "./components/Category";
 
 function App() {
     return (
         <Container>
             <Nav/>
             <Banner/>
+            <Category/>
         </Container>
     );
 }
