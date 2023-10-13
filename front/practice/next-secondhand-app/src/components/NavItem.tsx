03f399245dd64ff6ee48987fbaf4eb598aaacbb4
renechoi <115696395+renechoi@users.noreply.github.com>
2023-10-13 17:24:59 +0900

    feat: nextjs app structure, nav 구현

---
+++ front/practice/next-secondhand-app/src/components/NavItem.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+export const NavItem = ({mobile}: { mobile?: boolean }) => {
+    return (
+        <ul className={'text-md justify-center flex gap-4 w-full items-center ${mobile && "flex-col h-full"}'}>
+            <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/admin'>Admin</Link></li>
+            <li className='py-2 text-center border-b-4 cursor-pointer'><Link href='/user'>User</Link></li>
+            <li className='py-2 text-center border-b-4 cursor-pointer'><button>SignIn</button></li>
+            <li className='py-2 text-center border-b-4 cursor-pointer'><button>SignOut</button></li>
+        </ul>
+    );
+};
