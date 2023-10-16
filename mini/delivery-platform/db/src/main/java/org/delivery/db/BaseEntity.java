d7aa84fa07484b31cb83181cd25c533ee3bf6637
renechoi <115696395+renechoi@users.noreply.github.com>
2023-10-16 15:31:57 +0900

    feat: spring security를 적용한 가맹점 서버 개발

--- mini/delivery-platform/db/src/main/java/org/delivery/db/BaseEntity.java
+++ mini/delivery-platform/db/src/main/java/org/delivery/db/BaseEntity.java
@@ -1,17 +1,20 @@
 package org.delivery.db;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 @MappedSuperclass
 @Data
 @SuperBuilder
 @NoArgsConstructor
+@AllArgsConstructor
 public class BaseEntity {
 
     @Id
