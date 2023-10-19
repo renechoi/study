c483c724446470806e0ceab8ff5f905d7a625e60
renechoi <115696395+renechoi@users.noreply.github.com>
2023-10-19 20:42:10 +0900

    feat: flux operator example - map, filter, take, flatmap

---
+++ webflux/reactor-practice/src/main/java/Operator1.java
@@ -0,0 +1,50 @@
+
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class Operator1 {
+    // map
+    public Flux<Integer> fluxMap() {
+        return Flux.range(1, 5)
+                .map(i -> i * 2)
+                .log();
+    }
+
+    // filter
+    public Flux<Integer> fluxFilter() {
+        return Flux.range(1, 10)
+                .filter(i -> i > 5)
+                .log();
+    }
+
+    // take
+    public Flux<Integer> fluxFilterTake() {
+        return Flux.range(1, 10)
+                .filter(i -> i > 5)
+                .take(3)
+                .log();
+    }
+
+    // flatmap -> 비동기 순서 비보장
+    // 일종의 2중 for 반복문
+    public Flux<Integer> fluxFlatMap() {
+        return Flux.range(1, 10)
+                .flatMap(i -> Flux.range(i * 10, 10)
+                        .delayElements(Duration.ofMillis(100)))
+                .log();
+    }
+
+    // 일종의 구구단 코드
+    public Flux<Integer> fluxFlatMap2() {
+        return Flux.range(1, 9)
+                .flatMap(i -> Flux.range(1, 9)
+                        .map(j -> {
+                            System.out.printf("%d * %d = %d\n", i, j, i * j);
+                            return i * j;
+                        })
+                );
+//                .log();
+    }
+
+}
