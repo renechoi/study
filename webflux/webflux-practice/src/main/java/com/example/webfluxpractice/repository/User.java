23bfc17e6860c5d525b3bb88df56bc2f365c678c
renechoi <115696395+renechoi@users.noreply.github.com>
2023-10-21 08:01:02 +0900

    feat: r2dbc를 사용하도록 user entity 변경

--- webflux/webflux-practice/src/main/java/com/example/webfluxpractice/repository/User.java
+++ webflux/webflux-practice/src/main/java/com/example/webfluxpractice/repository/User.java
@@ -3,17 +3,26 @@ package com.example.webfluxpractice.repository;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
-// entity
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
+@Table("users")
 public class User {
+    @Id
     private Long id;
     private String name;
     private String email;
+    @CreatedDate
     private LocalDateTime createdAt;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
