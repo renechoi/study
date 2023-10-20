0edc8858e820f754eaae9570808993280b4c6623
renechoi <115696395+renechoi@users.noreply.github.com>
2023-10-21 07:21:21 +0900

    feat: user service logic성

---
+++ webflux/webflux-practice/src/main/java/com/example/webfluxpractice/service/UserService.java
@@ -0,0 +1,42 @@
+package com.example.webfluxpractice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import com.example.webfluxpractice.repository.User;
+import com.example.webfluxpractice.repository.UserRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public Mono<User> create(String name, String email) {
+        return userRepository.save(User.builder().name(name).email(email).build());
+    }
+
+    public Flux<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Mono<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Mono<Integer> deleteById(Long id) {
+        return userRepository.deleteById(id);
+    }
+
+    // flatmap으로 새로운 스트림을 생성
+    public Mono<User> update(Long id, String name, String email) {
+        return userRepository.findById(id)
+                .flatMap(u -> {
+                    u.setName(name);
+                    u.setEmail(email);
+                    return userRepository.save(u);
+                });
+    }
+}
