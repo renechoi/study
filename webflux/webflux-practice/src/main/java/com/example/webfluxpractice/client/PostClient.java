5c5ded16b6c91e606d477186479a3a49caf4da71
renechoi <115696395+renechoi@users.noreply.github.com>
2023-10-21 08:04:39 +0900

    feat: post client 구현

---
+++ webflux/webflux-practice/src/main/java/com/example/webfluxpractice/client/PostClient.java
@@ -0,0 +1,29 @@
+package com.example.webfluxpractice.client;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.example.webfluxpractice.dto.PostResponse;
+
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class PostClient {
+    private final WebClient webClient;
+    private final String url = "http://127.0.0.1:8090";
+
+    public Mono<PostResponse> getPost(Long id) {
+        String uriString = UriComponentsBuilder.fromHttpUrl(url)
+                .path("/posts/%d".formatted(id))
+                .buildAndExpand()
+                .toUriString();
+
+        return webClient.get()
+                .uri(uriString)
+                .retrieve()
+                .bodyToMono(PostResponse.class);
+    }
+}
