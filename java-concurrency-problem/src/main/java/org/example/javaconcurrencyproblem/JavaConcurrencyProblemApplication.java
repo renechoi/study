6a8b48ea67ccf3e10fb7b1b77cd4c1d1172e9336
renechoi <115696395+renechoi@users.noreply.github.com>
2024-03-19 19:44:08 +0900

    feat: project init

---
+++ java-concurrency-problem/src/main/java/org/example/javaconcurrencyproblem/JavaConcurrencyProblemApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaconcurrencyproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConcurrencyProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConcurrencyProblemApplication.class, args);
+	}
+
+}
