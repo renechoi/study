befa67676f4b98665d5796916c2356fad81f7264
renechoi <115696395+renechoi@users.noreply.github.com>
2024-06-22 20:59:40 +0900

    feat: initialize project

---
+++ hhplus-lecture-application-system/src/main/java/io/hhpluslectureapplicationsystem/HhplusLectureApplicationSystemApplication.java
@@ -0,0 +1,13 @@
+package io.hhpluslectureapplicationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusLectureApplicationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusLectureApplicationSystemApplication.class, args);
+	}
+
+}
---
+++ hhplus-lecture-application-system/src/test/java/io/hhpluslectureapplicationsystem/HhplusLectureApplicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhpluslectureapplicationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusLectureApplicationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
